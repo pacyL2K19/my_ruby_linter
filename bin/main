b2cb5864dab8bfe48f0320ac05da2ae47bcd34b8
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-01-27 22:20:40 +0100

    Test the file_validator method

---
+++ bin/main
@@ -0,0 +1,7 @@
+require_relative '../lib/modules/file_reader.rb'
+
+include FileReader
+puts "Please enter the file of the file to be checked"
+file_path = gets.chomp
+
+puts FileReader.file_validator(file_path)
--- lib/modules/file_reader.rb
+++ lib/modules/file_reader.rb
@@ -1,9 +1,14 @@
 require 'colorize'
 module FileReader
   def file_validator(file_path)
-    return "The specified file does not exist, pleqse enter a correct one".colorize(:color => :white, :background => :red) unless File.exist?(file_path)
-    return "The file extension is not allowed, please put a .rb file to be checked".colorize(:color => :white, :background => :red) unless File.extname == ".rb"
-    return "This file is empty".colorize(:color => :white, :background => :red) unless File.zero?(file_path)
-    true
+    # p "One"
+    return " The specified file does not exist, pleqse enter a correct one ".colorize(:color => :white, :background => :red) unless File.exist?(file_path)
+    # p "two"
+    return " The file extension is not allowed, please put a .rb file to be checked ".colorize(:color => :white, :background => :red) unless File.extname(file_path) == ".rb"
+    # p "Three"
+    return " This file is empty ".colorize(:color => :white, :background => :red) unless File.zero?(file_path)
+    # p "four"
+    # File.size(file_path)
+    return true
   end
 end
