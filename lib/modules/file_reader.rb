7ca2ed35e62c9e244f8fd671037653a97ef08ba7
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-01-27 21:15:59 +0100

    Implement the file_reader file, to check info of the file and its validity

--- lib/modules/file_reader.rb
+++ lib/modules/file_reader.rb
@@ -1,3 +1,9 @@
+require 'colorize'
 module FileReader
-  
+  def file_validator(file_path)
+    return "The specified file does not exist, pleqse enter a correct one" unless File.exist?(file_path)
+    return "The file extension is not allowed, please put a .rb file to be checked" unless File.extname == ".rb"
+    return "This file is empty" unless File.zero?(file_path)
+    true
+  end
 end
