e61d5a91a9d134efe829086e8f1fbb8b3a92eab3
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-01-28 18:00:24 -0100

    Update the checker module

--- lib/checker.rb
+++ lib/checker.rb
@@ -1,4 +1,8 @@
+require_relative './modules/checker_module'
+require_relative './modules/file_reader'
 class Checker
+  include Checker_Module
+  include FileReader
   def initialize
     
   end
--- lib/modules/file_reader.rb
+++ lib/modules/file_reader.rb
@@ -2,14 +2,9 @@ require 'colorize'
 module FileReader
   def file_validator(file_path)
     danger = :red
-    # p "One"
     return " The specified file does not exist, pleqse enter a correct one ".colorize(:color => :white, :background => danger) unless File.exist?(file_path)
-    # p "two"
     return " The file extension is not allowed, please put a .rb file to be checked ".colorize(:color => :white, :background => danger) unless File.extname(file_path) == ".rb"
-    # p "Three"
     return " This file is empty ".colorize(:color => :white, :background => danger) unless File.zero?(file_path)
-    # p "four"
-    # File.size(file_path)
     return true
   end
 end
