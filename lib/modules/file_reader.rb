a74655e8fcde282e07ad8ed3013270ee30f36fe0
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-02-03 21:04:53 +0100

    inr update

--- lib/modules/file_reader.rb
+++ lib/modules/file_reader.rb
@@ -1,13 +1,7 @@
 module FileReader
   def file_validator(file_path)
-    danger = :red
-    warning = :yellow
-    unless File.exist?(file_path)
-      return ' The specified file does not exist, please enter a correct one '
-    end
-    unless File.extname(file_path) == '.rb'
-      return ' The file extension is not allowed, please put a .rb file to be checked '
-    end
+    return ' The specified file does not exist, please enter a correct one ' unless File.exist?(file_path)
+    return ' The file extension is not allowed, please put a .rb file to be checked ' unless File.extname(file_path) == '.rb'
     return ' This file is empty ' if File.zero?(file_path)
 
     true
