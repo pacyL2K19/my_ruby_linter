9e068fe66b661ca1762968fa0e231e2099b2cf93
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-02-03 20:59:58 +0100

    Minor update

--- lib/modules/file_reader.rb
+++ lib/modules/file_reader.rb
@@ -1,17 +1,14 @@
-require 'colorize'
 module FileReader
   def file_validator(file_path)
     danger = :red
     warning = :yellow
     unless File.exist?(file_path)
-      return ' The specified file does not exist, please enter a correct one '.colorize(color: :white,
-                                                                                        background: danger)
+      return ' The specified file does not exist, please enter a correct one '
     end
     unless File.extname(file_path) == '.rb'
-      return ' The file extension is not allowed, please put a .rb file to be checked '.colorize(color: :white,
-                                                                                                 background: danger)
+      return ' The file extension is not allowed, please put a .rb file to be checked '
     end
-    return ' This file is empty '.colorize(color: :black, background: warning) if File.zero?(file_path)
+    return ' This file is empty ' if File.zero?(file_path)
 
     true
   end
